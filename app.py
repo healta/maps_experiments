b05412975294d052b9c0cce19a713abc8e926495
healta <dimitar.vasilev.dimitrov95@gmail.com>
2023-07-15 16:17:39 +0300

    Hello world

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+import folium
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    map = folium.Map(location = [42.43278, 25.64194])
+    return map._repr_html_()
+
+if __name__ == '__main__':
+    app.run(debug=True)
